8c82412271db2bb9b797d67ae5ba6b6b61432331
Hanniesweety <hanniesweetyh@gmail.com>
2026-05-27 22:46:19 +0530

    feat: implement user registration API with password hashing

---
+++ backend/controllers/authController.js
@@ -0,0 +1,46 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
+export const registerUser = async (req, res) => {
+  try {
+
+    const { name, email, password, role } = req.body;
+
+    // check existing user
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // create new user
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        role: newUser.role,
+      },
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      message: error.message,
+    });
+
+  }
+};
