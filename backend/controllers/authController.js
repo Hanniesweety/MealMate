bca9fa467afeeae3bad8e2e24e75bc61790d17a6
Hanniesweety <hanniesweetyh@gmail.com>
2026-05-28 09:46:55 +0530

    feat: implement JWT login authentication and secure user validation

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,11 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
+
+// REGISTER USER
 export const registerUser = async (req, res) => {
+
   try {
 
     const { name, email, password, role } = req.body;
@@ -43,4 +47,64 @@ export const registerUser = async (req, res) => {
     });
 
   }
+
+};
+
+
+// LOGIN USER
+export const loginUser = async (req, res) => {
+
+  try {
+
+    const { email, password } = req.body;
+
+    // check user
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    // compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    // generate token
+    const token = jwt.sign(
+      {
+        id: user._id,
+        role: user.role,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      message: error.message,
+    });
+
+  }
+
 };
