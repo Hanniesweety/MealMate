02da64c68cfb070f167f06d3e45f49fecd29d7be
Hanniesweety <hanniesweetyh@gmail.com>
2026-05-29 23:01:45 +0530

    feat: implement JWT authentication with secure login system

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
+
 import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 
@@ -12,5 +14,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
+
+app.use("/api/user", userRoutes);
 
 export default app;
