29c04065ca4bdbd8cad45dd21dd39428d761f53b
Hanniesweety <hanniesweetyh@gmail.com>
2026-05-29 23:12:32 +0530

    feat: create restaurant schema with geospatial indexing support

---
+++ Restaurant.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+
+const restaurantSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+    },
+
+    cuisine: {
+      type: String,
+    },
+
+    image: {
+      type: String,
+    },
+
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    address: {
+      type: String,
+    },
+
+    location: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        default: "Point",
+      },
+
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
+    },
+
+    rating: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  {
+    timestamps: true,
+  }
+);
+
+// GeoSpatial Index
+restaurantSchema.index({
+  location: "2dsphere",
+});
+
+const Restaurant = mongoose.model(
+  "Restaurant",
+  restaurantSchema
+);
+
+export default Restaurant;
